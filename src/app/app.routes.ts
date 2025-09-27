abba6710d3c1d59751107b55d9886ada02d9ae40
Kim Phú <kimphu111@gmail.com>
2025-09-27 13:09:47 +0700

    done ui ux home register login

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,10 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { HeaderComponent } from './pages/header/header.component';
 import { AboutComponent } from './pages/about/about.component';
-import { MenuComponent } from './pages/menu.component/menu.component';
+import { MenuComponent } from './pages/menu/menu.component';
 import { LoginComponent } from './pages/login/login.component';
-
+import {FooterComponent} from "./pages/footer/footer.component";
+import {RegisterComponent} from './pages/register/register.component';
 export const routes: Routes = [
   {
     path: '',
@@ -13,10 +14,11 @@ export const routes: Routes = [
       { path: 'home', component: HomeComponent },
       { path: 'menu', component: MenuComponent },
       { path: 'about', component: AboutComponent },
-      { path: 'login', component: LoginComponent },
       { path: '', redirectTo: 'home', pathMatch: 'full' },
+      { path: 'footer', component: FooterComponent}
     ],
   },
-
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
   { path: '**', redirectTo: 'home' },
 ];
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,10 +1,11 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Router, RouterLink, RouterLinkActive } from '@angular/router';
+import {FooterComponent} from '../footer/footer.component';
 
 @Component({
   selector: 'app-root',
-  imports: [CommonModule],
+  imports: [CommonModule, FooterComponent],
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
 })
@@ -16,9 +17,24 @@ export class HomeComponent implements OnInit, OnDestroy {
     'assets/Carousel4.png',
   ];
 
+  menuItems = [
+    { img: 'assets/AboutUs1.png', name: 'Black Coffee', description: 'Prepared by forcing hot water under high pressure through finely ground coffee.\n', cost: "20.000đ" },
+    { img: 'assets/AboutUs1.png', name: 'Black Coffee', description: 'Prepared by forcing hot water under high pressure through finely ground coffee.\n', cost: "20.000đ" },
+    { img: 'assets/AboutUs1.png', name: 'Black Coffee', description: 'Prepared by forcing hot water under high pressure through finely ground coffee.\n', cost: "20.000đ" },
+    { img: 'assets/AboutUs1.png', name: 'Black Coffee', description: 'Prepared by forcing hot water under high pressure through finely ground coffee.\n', cost: "20.000đ" },
+    { img: 'assets/AboutUs1.png', name: 'Black Coffee', description: 'Prepared by forcing hot water under high pressure through finely ground coffee.\n', cost: "20.000đ" },
+    { img: 'assets/AboutUs1.png', name: 'Black Coffee', description: 'Prepared by forcing hot water under high pressure through finely ground coffee.\n', cost: "20.000đ" },
+
+
+    // thêm các món khác
+  ];
+
   currentIndex = 0;
   intervalMs = 2000;
   private timerId: any = null;
+
+  currentMenuIndex = 0;
+  itemsPerPage = 3;
 
   get trackTransform(): string {
     return `translateX(-${this.currentIndex * 100}%)`;
@@ -59,5 +75,22 @@ export class HomeComponent implements OnInit, OnDestroy {
   goTo(i: number): void {
     this.currentIndex = i;
     this.play();
+  }
+
+  //render menu items
+  get menuTransform() {
+    return `translateX(-${(this.currentMenuIndex * 100) / this.itemsPerPage}%)`;
+  }
+
+  nextMenu() {
+    if (this.currentMenuIndex < this.menuItems.length - this.itemsPerPage) {
+      this.currentMenuIndex++;
+    }
+  }
+
+  prevMenu() {
+    if (this.currentMenuIndex > 0) {
+      this.currentMenuIndex--;
+    }
   }
 }
